97962a3a091f8232d44da84d02338528682b54dd
hasanbozacioglu <bozacihasan@gmail.com>
2015-10-28 21:51:55 +0200

    Hasan/Test.java

--- IdealProject/src/com/ideal/Hasan/Test.java
+++ IdealProject/src/com/ideal/Hasan/Test.java
@@ -1,11 +1,13 @@
 package com.ideal.Hasan;
 
-public class Test {
+public class Test extends Object{	// Javada Her sýnýf Object Classýndan extend edilir. Bu retim 'extends Object' yazýlmasada olur.
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TEST
-
+		System.out.println("ideal");
 	}
 
 }
+
+
